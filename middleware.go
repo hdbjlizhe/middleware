3f59103f93bc667366f1d407d7c4c62d4d753556
hdbjlizhe <hdbjlizhe@hotmail.com>
2024-02-05 09:56:36 +0800

    Update middleware.go

--- middleware.go
+++ middleware.go
@@ -507,7 +507,7 @@ func (s *Sender) Listen(timeout int) string {
 		"timeout":  timeout,
 	}
 	body, _ := json.Marshal(params)
-	resp, _ := httplib.Post(localUrl()+"/listen").Header("Content-Type", "application/json").Body(body).Bytes()
+	resp, _ := httplib.Post(localUrl()+"/listen").Header("Content-Type", "application/json").Body(body).SetTimeout(time.Millisecond * time.Duration(timeout)).Bytes()
 	rlt, _ := jsonparser.GetString(resp, "data")
 	return rlt
 }
